0b02e049d7b2ab0fb7f1e8d6297d8985d3582004
Jeef <jeef@example.com>
2026-08-14 22:52:38 -0500

    Updated inspections for subdirectories

--- sync.bat
+++ sync.bat
@@ -1,3 +1,3 @@
-rclone sync jeefnet:mods/ libs/ --include "*.jar" -u -v --inplace
-rclone copy jeefnet:jet/scripts src/main/kotlin/ --include "*.kts" -u -v --inplace
-rclone copy src/main/kotlin/ jeefnet:jet/scripts --include "*.kts" -u -v --inplace
+rclone sync testserver:mods/ libs/ --include "*.jar" -u -v --inplace
+rclone copy testserver:jet/scripts src/main/kotlin/ --include "*.kts" -u -v --inplace
+rclone copy src/main/kotlin/ testserver:jet/scripts --include "*.kts" -u -v --inplace
--- sync.sh
+++
@@ -1,3 +0,0 @@
-rclone sync jeefnet:mods/ libs/ --include "*.jar" -u -v --inplace
-rclone copy jeefnet:jet/scripts src/main/kotlin/ --include "*.kts" -u -v --inplace
-rclone copy src/main/kotlin/ jeefnet:jet/scripts --include "*.kts" -u -v --inplace
